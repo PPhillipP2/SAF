6aa04a411511927e9baed927b6f2fd4ae67353c7
PPhillipP2 <phillip.phan.676@my.csun.edu>
2024-04-24 17:09:46 -0700

    initial data clean up, need revisions

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 import spacy
 import pandas as pd
+from spacy.lang.en.stop_words import STOP_WORDS
+import re
 
-# df = pd.read_csv(r'C:\Users\phanp\Downloads\IMDB')
+df = pd.read_csv(r'Data/IMDB_Dataset.csv')
 
 # Load the English tokenizer and language model
 nlp = spacy.load('en_core_web_sm')
@@ -9,12 +11,19 @@ nlp = spacy.load('en_core_web_sm')
 # Test for tokenization
 test = "I love love, what the frick, dog water, long-short, crapbucket, U.K., the is"
 tokens = [token.text for token in nlp(test)]
-print(test)
+print(tokens)
 
-cleanTest = [token.text.lower() for token in test if not token.is_punct and not token.is_stop]
-print(cleanTest)
+# Clean up data
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    tokens2 = nlp(text)
+    tokens2 = [token.text for token in tokens2 if token.text]
+    return tokens2
 
-# Process the text with spaCy
-# doc = nlp(df['IMDB Dataset'])
+df['clean_review'] = df['review'].apply(clean_text)
 
-# Clean tokens
+
+# Print the tokens
+print(df['clean_review'].iloc[0])
+
