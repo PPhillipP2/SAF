4c0096c032d5f13e7707b239822da8d1daadda8a
Khromage <khromage@gmail.com>
2024-04-30 13:02:06 -0700

    spaCy python file comments

--- Main.py
+++ Main.py
@@ -6,58 +6,69 @@ from sklearn.metrics import accuracy_score, f1_score
 import numpy as np
 import re
 
-# load dataset file
+# Result display formatting
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', 100)
 
-pd.set_option('display.max_rows', None)  # Show all rows
-pd.set_option('display.max_columns', None)  # Show all columns
-pd.set_option('display.max_colwidth', 100)  # Show full width of each column
+# Prefer GPU training over CPU if available
+activated = spacy.prefer_gpu()
 
-# Load the English tokenizer and language model
-activated = spacy.require_gpu()
+# Load spaCy English transformer model
 nlp = spacy.load('en_core_web_trf')
+
+# Append SpacyTextBlob to spaCy pipeline for sentiment analysis
 spacy_text_blob = SpacyTextBlob(nlp)
 nlp.add_pipe('spacytextblob')
 
 
-# Clean up data
+# Clean input data
 def clean_text(text):
     # remove html line breaks
     text = re.sub(r'<br\s*/?>', '', text)
+
     #remove non-alphanumeric characters
-    #text = re.sub(r"[^a-zA-Z0-9]", ' ', text)
+    text = re.sub(r"[^a-zA-Z0-9]", ' ', text)
+
     # Remove extra whitespaces
     text = re.sub(r'\s+', ' ', text).strip()
 
-    # Lemmatization with SpaCy
+    # Lemmatization using SpaCy tokens
     cleaning_doc = nlp(text)
     lemmatized_text = ' '.join([token.lemma_ for token in cleaning_doc])
+
+    # return cleaned and lemmatized text
     return lemmatized_text
 
 
 
 # Function to analyze sentiment using SpaCyTextBlob
 def analyze_sentiment(text):
+    # Tokenize and process input
     doc = nlp(text)
-    # filter features/token properties here
 
-
-    # Return the sentiment
+    # Return the sentiment polarity
     return doc._.blob.sentiment.polarity
 
 
 # Load data
 def load_data(filepath):
+    # Using pandas to load dataset
     data = pd.read_csv(filepath)
     data['review'] = data['review'].apply(clean_text)
+
+    # Return pandas dataframe
     return data
 
 
 def perform_sentiment_analysis(filepath):
     data = load_data(filepath)
-    kf = KFold(n_splits=7)  # 5-fold cross-validation
+    # 7-fold cross-validation
+    kf = KFold(n_splits=7)
     accuracies = []
     f1_scores = []
-    all_predictions = []  # List to store all predictions
+    # List to store predictions
+    all_predictions = []
 
     # Prepare data
     for train_index, test_index in kf.split(data):
@@ -78,7 +89,7 @@ def perform_sentiment_analysis(filepath):
         f1_scores.append(f1)
 
 
-    # Convert predictions list to DataFrame and save or display
+    # Convert predictions list to DataFrame, save, and display results
     predictions_df = pd.DataFrame(all_predictions)
     predictions_df.to_csv('movie_review_predictions.csv', index=False)
     print(predictions_df.head(15))  # Optionally print the first few rows
@@ -87,32 +98,6 @@ def perform_sentiment_analysis(filepath):
     print(f"Average Model Accuracy: {np.mean(accuracies):.4f}")
     print(f"Average Model F1 Score: {np.mean(f1_scores):.4f}")
 
-# If this script is the main program being executed
+# Model training
 if __name__ == "__main__":
     perform_sentiment_analysis('Data/IMDB Dataset - 10000.csv')
-
-
-
-
-"""
-df = pd.read_csv(r'Data/IMDB Dataset MINIMIZED.csv')
-
-
-review_set = df.iloc[:, 0].tolist()
-
-# spacy default pipeline
-print("Default spacy tokens:")
-for review in review_set:
-    print(review)
-    doc = nlp(clean_text(review))
-    #doc = nlp(review)
-    # all desired properties from tokens picked here
-    print([[token.text, token.pos_] for token in doc if not token.is_stop and not token.is_punct])
-
-    #for sentence in doc.sents:
-    #    print(sentence._.blob.sentiment)
-
-    print(doc._.blob.sentiment)
-
-    print()
-"""
